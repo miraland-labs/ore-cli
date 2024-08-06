19114130a7ab249d3326fbf7285c617e4bb908e6
Miraland Labs <miraland.labs@gmail.com>
2024-08-06 14:28:23 +0800

    revert confirm delay 0, set gateway delay 1500

--- src/send_and_confirm.rs
+++ src/send_and_confirm.rs
@@ -24,13 +24,11 @@ const MIN_SOL_BALANCE: f64 = 0.005;
 
 const RPC_RETRIES: usize = 0;
 const _SIMULATION_RETRIES: usize = 4;
-const GATEWAY_RETRIES: usize = 75; // MI, vanilla: 150, keep retries * delay same
+const GATEWAY_RETRIES: usize = 30; // MI, vanilla: 150, keep retries * delay same
 const CONFIRM_RETRIES: usize = 1;
 
-// MI, vanilla, millis
-// const CONFIRM_DELAY: u64 = 0;
-const CONFIRM_DELAY: u64 = 1000;
-const GATEWAY_DELAY: u64 = 600; // MI, vanilla: 300, keep retries * delay same
+const CONFIRM_DELAY: u64 = 0;
+const GATEWAY_DELAY: u64 = 1500; // MI, vanilla: 300, keep retries * delay same
 
 pub enum ComputeBudget {
     Dynamic,
